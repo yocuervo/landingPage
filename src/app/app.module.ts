77c5775680481b41480b7580ca982d9c2ccc9ac6
Cuervo <yo.cuervo.yo@gmail.com>
2020-09-26 01:46:04 -0300

    separando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Slide1Component } from './slide1/slide1.component';
+import { Slide2Component } from './slide2/slide2.component';
+import { Slide3Component } from './slide3/slide3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Slide1Component,
+    Slide2Component,
+    Slide3Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/slide1/slide1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slide1',
+  templateUrl: './slide1.component.html',
+  styleUrls: ['./slide1.component.css']
+})
+export class Slide1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  irA() {
+    window.location.href = 'https://gmail.us2.list-manage.com/subscribe/post?u=a8b4a41febe74ba5755bd820e&id=54ee4535cd';
+   }
+
+}
